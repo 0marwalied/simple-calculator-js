b30d31dd533094eecdaa4fcce4b7c8c7811109c0
omar_walied <96862135+0marwalied@users.noreply.github.com>
2025-02-06 22:32:39 +0200

    Handling the but of the del funciton on script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,40 @@
 var screen = document.querySelector(".screen");
 var screenContent = "";
+var lastOpeeration = "";
 
 function appendScreen(char) {
   screenContent += char;
   screen.innerHTML = screenContent;
+  lastOpeeration = "appendScreen";
 }
 
 function deleteScreen() {
-  if (screenContent == "Error") {
+  if (
+    lastOpeeration == "Error" ||
+    lastOpeeration == "calculate" ||
+    lastOpeeration == "clearScreen"
+  ) {
     screenContent = "";
   } else {
     screenContent = screenContent.slice(0, -1);
   }
   screen.innerHTML = screenContent;
+  lastOpeeration = "deleteScreen";
 }
 
 function clearScreen() {
   screenContent = "";
   screen.innerHTML = screenContent;
+  lastOpeeration = "clearScreen";
 }
 
 function calculate() {
   try {
+    lastOpeeration = "calculate";
     screenContent = eval(screenContent);
     screen.innerHTML = screenContent;
   } catch (e) {
+    lastOpeeration = "Error";
     screenContent = "Error";
     screen.innerHTML = screenContent;
   }
